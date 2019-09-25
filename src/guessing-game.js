3ebd83fc92106c38873ac2d5c9ac7b53625c2af5
serh <serh169@gmail.com>
2019-09-25 17:52:28 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,35 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minIndx = 0;
+        this.maxIndx = 0;
+        this.midIndex = 0;
+        this.arr = [];
+
+    }
 
     setRange(min, max) {
 
+        for (let i = min;  i <= max ; i++ ) {
+            this.arr.push(i) ;
+        }
+
+        this.minIndx = 0;
+        this.maxIndx = this.arr.length-1;
+
     }
 
-    guess() {
-
+    guess(type = '') {
+        let midIndex = Math.round((this.minIndx+this.maxIndx) / 2);
+        this.midIndex = midIndex;
+        return this.arr[midIndex];
     }
 
     lower() {
-
+        this.maxIndx = this.midIndex;
     }
 
     greater() {
-
+        this.minIndx = this.midIndex;
     }
 }
 
